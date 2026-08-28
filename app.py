ae522f4435eb0a9ce9cbb77bcc425b942115cedd
Paolam1903 <sernapaola922@gmail.com>
2026-08-28 07:28:11 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ with col2:
 
     st.markdown("""
     <h1 style='margin-bottom:0'>
-    REPORTE DIARIO DE TERMINALES AL 26 DE AGOSTO
+    REPORTE DIARIO DE TERMINALES AL 27 DE AGOSTO
     </h1>
 
     <h4 style='color:gray;margin-top:0'>
