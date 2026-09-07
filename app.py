fc2d649f8885e65e086d8c729d93d7769f74f3c7
Paolam1903 <sernapaola922@gmail.com>
2026-09-07 08:32:17 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ with col2:
 
     st.markdown("""
     <h1 style='margin-bottom:0'>
-    REPORTE DIARIO DE EQUIPOS AL 3 DE SEPTIEMBRE
+    REPORTE DIARIO DE EQUIPOS AL 6 DE SEPTIEMBRE
     </h1>
 
     <h4 style='color:gray;margin-top:0'>
